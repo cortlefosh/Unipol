96eee399df7be7673d06b51ab5f87f555fde7af3
cortlefosh <33757621+cortlefosh@users.noreply.github.com>
2017-11-18 09:41:57 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<?php require head.php; ?>
+	<title>Welcome to Unipol!</title>
+	<link type="text/css" rel="stylesheet" href="css/index.css">
+	<script src="js/index.js"></script>
+</head>
+<body>
+
+
+
+</body>
+</html>
