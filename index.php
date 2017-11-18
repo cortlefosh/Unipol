691682e39c03ab5b072ca036bc48e42075e6c390
cortlefosh <33757621+cortlefosh@users.noreply.github.com>
2017-11-18 23:05:31 +0100

    Basic Structure
    Create template and datanet PHP files, add basic login process

--- index.php
+++ index.php
@@ -1,13 +1,35 @@
+<!DOCTYPE html>
+<?php 
+	session_start(); 
+	if (isset($_POST["name"])) {
+		$_SESSION["name"] = $_POST["name"];
+		$username = $_POST["name"];
+	}
+?>
 <html>
 <head>
-	<?php require head.php; ?>
+	<?php //require templates\head.php; ?>
 	<title>Welcome to Unipol!</title>
 	<link type="text/css" rel="stylesheet" href="css/index.css">
 	<script src="js/index.js"></script>
 </head>
 <body>
-
-
-
+	<?php //require templates\background.php; ?>
+	<h1 id="welcomeMessage">Welcome to Unipol<?php
+		if (isset($username)) {echo ", " . $username;}
+	?>!</h1>
+	<p id="unipolDesc">Unipol is ipsum lorem dolor</p>
+	<?php if (!isset($username)) { echo
+	"<form id='nameInput' action='index.php' method='post'>
+		<p>Enter your name here to access the Unipol datanet!</p>
+		<input type='text' name='name' value='Enter Name'>
+		<input type='submit' name='submit' value='Submit'>
+	</form>";
+	} else { echo
+	'<form id="accessUnipol" action="datanet.php" method="post">
+		<p>Click here to access the Unipol datanet!</p>
+		<input type="submit" value="Access datanet">
+	</form>';
+	} ?>
 </body>
 </html>
